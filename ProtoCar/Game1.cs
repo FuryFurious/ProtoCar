faedda0c54fdca77f1d7e58c6d1c8651d4a0d24c
FuryFurious <ricu05@yahoo.de>
2014-04-04 16:17:56 +0200

    first commit

---
+++ ProtoCar/Game1.cs
@@ -0,0 +1,29 @@
+﻿using SharpDX.Toolkit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtoCar
+{
+    class Game1 : Game
+    {
+        GraphicsDeviceManager gManager;
+
+        public Game1()
+        {
+            this.gManager = new GraphicsDeviceManager(this);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ ProtoCar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtoCar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game1 game1 = new Game1();
+            game1.Run();
+        }
+    }
+}
